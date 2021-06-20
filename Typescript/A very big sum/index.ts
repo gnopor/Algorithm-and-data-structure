245a4bb1f18b13f820db5dda2967e90af8a44aab
gnopor <mayoublaise@gmail.com>
2021-06-20 21:42:48 +0100

    solve a very big sum

---
+++ Typescript/A very big sum/index.ts
@@ -0,0 +1,12 @@
+const array = [5, "1000000001 1000000002 1000000003 1000000004 1000000005"];
+
+// 1 <= n <= 10;
+
+// 0 <= ar[i] <= 1010;
+
+function aVeryBigSum(ar: number[]): number {
+  // const n = ar[0]
+  // const numbers = ar[1].toString().split(" ")
+
+  return ar.reduce((prev: number, current: number) => prev + current);
+}
