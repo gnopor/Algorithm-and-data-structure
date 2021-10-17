612e9cbffc15bede2d43b5a513a6f71fbd30e9d0
gnopor <mayoublaise@gmail.com>
2021-10-17 19:04:45 +0100

    add divisible sum pairs chalenge

---
+++ Typescript/src/Divisible Sum Pairs/index.ts
@@ -0,0 +1,17 @@
+function divisibleSumPairs(n: number, k: number, ar: number[]): number {
+  let num_pairs = 0;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      const result = (ar[i] + ar[j]) % k;
+      result === 0 && num_pairs++;
+    }
+  }
+
+  return num_pairs;
+}
+
+// test
+// console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]));
+
+module.exports = divisibleSumPairs;
---
+++ Typescript/src/Divisible Sum Pairs/test/divisibleSumPairs.test.js
@@ -0,0 +1,11 @@
+const divisibleSumPairs = require("../index.js");
+
+describe("Divisible Sum Pairs", () => {
+  it("test divisible sum pairs with correct data", () => {
+    const n = 6;
+    const k = 3;
+    const arr = [1, 3, 2, 6, 1, 2];
+    const expected_result = 5;
+    expect(divisibleSumPairs(n, k, arr)).toBe(expected_result);
+  });
+});
