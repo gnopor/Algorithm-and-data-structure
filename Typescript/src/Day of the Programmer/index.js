341e048fbc07ba055e53c10160f70ab2154813cf
gnopor <mayoublaise@gmail.com>
2021-10-25 05:44:33 +0100

    add Day of programmer

---
+++ Typescript/src/Day of the Programmer/index.js
@@ -0,0 +1,17 @@
+function dayOfProgrammer(year) {
+    var is_leap_years = false;
+    if (year === 1918) {
+        return "26.09.1918";
+    }
+    if (year < 1918) {
+        is_leap_years = year % 4 === 0;
+    }
+    else {
+        is_leap_years = year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+    }
+    return (is_leap_years ? 12 : 13) + ".09." + year;
+}
+// test function
+// const year = 2016;
+// console.log(dayOfProgrammer(year));
+module.exports = dayOfProgrammer;
---
+++ Typescript/src/Day of the Programmer/index.ts
@@ -0,0 +1,20 @@
+function dayOfProgrammer(year: number): string {
+  let is_leap_years = false;
+
+  if (year === 1918) {
+    return "26.09.1918";
+  }
+
+  if (year < 1918) {
+    is_leap_years = year % 4 === 0;
+  } else {
+    is_leap_years = year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+  }
+
+  return `${is_leap_years ? 12 : 13}.09.${year}`;
+}
+
+// test function
+// const year = 2016;
+// console.log(dayOfProgrammer(year));
+module.exports = dayOfProgrammer;
