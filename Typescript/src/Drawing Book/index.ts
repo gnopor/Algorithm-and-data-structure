203baf8a13d17af62b4808d87f834f493ef09806
Gnop <mayoublaise@gmail.com>
2022-11-13 19:37:05 +0100

    update the Drawing book chanlenge to improve it score from 9.5 to 10 points

--- Typescript/src/Drawing Book/index.ts
+++ Typescript/src/Drawing Book/index.ts
@@ -1,12 +1,29 @@
-/**
- * ! Not totaly solved. Only get 9.60/10
- * @param n
- * @param p
- * @returns
- */
+export default function pageCount(n: number, p: number) {
+  const numPages = Math.floor(n / 2);
+  let frontCount = 0;
+  let backCount = 0;
 
-export default function pageCount(n: number, p: number): number {
-  return n - p > n / 2 ? Math.floor(p / 2) : Math.floor((n - p) / 2);
+  for (let i = 0; i <= numPages; i++) {
+    if (i * 2 <= p && i * 2 + 1 >= p) {
+      break;
+    }
+    frontCount += 1;
+  }
+
+  if (frontCount === 0) return 0;
+
+  for (let i = numPages; i >= 0; i--) {
+    if (i * 2 <= p && i * 2 + 1 >= p) {
+      break;
+    }
+    backCount += 1;
+  }
+
+  return frontCount > backCount ? backCount : frontCount;
 }
 
-// console.log(pageCount(5, 4));
+// // test
+// const n = 5;
+// const p = 4;
+
+// console.log(pageCount(n, p));
---
+++ Typescript/src/Sort Array copy/index.ts
@@ -0,0 +1,18 @@
+function sortArray(arr: number[]): number[] {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        const greater = arr[i];
+        arr[i] = arr[j];
+        arr[j] = greater;
+      }
+    }
+  }
+
+  return arr;
+}
+
+// test ---------
+
+const arr = [3, 233, 0, -123, 2000, -2, 9, 5, 2, 1, 2, -10];
+console.log(sortArray(arr));
