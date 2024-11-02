b60413bfd1fa3d1b8d72e57ff3033a7ffb8a284c
Blaise TAYOU <mayoublaise@gmail.com>
2024-11-02 15:41:27 +0100

    Add a new challenge: Highest value palingrome

--- Typescript/src/Highest Value Palindrome/test/highestValuePalindrome.test.ts
+++ Typescript/src/Highest Value Palindrome/test/highestValuePalindrome.test.ts
@@ -3,7 +3,7 @@ import highestValuePalindrome from "../index";
 describe("Cut The Sticks", () => {
   it("Function should return the highest value palindrome.", () => {
     const s = "11331";
-    const k = 1;
+    const k = 4;
     const expectedResult = "99399";
 
     expect(highestValuePalindrome(s, s.length, k)).toBe(expectedResult);
