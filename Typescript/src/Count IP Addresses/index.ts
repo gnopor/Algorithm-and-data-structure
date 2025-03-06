6957a01157d8229ee52b94ae27bb0f425134a4c3
Blaise TAYOU <mayoublaise@gmail.com>
2025-03-06 21:27:32 +0100

    Add a new challenge: Count IP Addresses

---
+++ Typescript/src/Count IP Addresses/index.ts
@@ -0,0 +1,29 @@
+export default function ipsBetween(start: string, end: string): number {
+  const [s1, s2, s3, s4] = start.split(".").map((v) => +v);
+  const [e1, e2, e3, e4] = end.split(".").map((v) => +v);
+
+  const startCountFromZero =
+    s1 * Math.pow(256, 3) +
+    s2 * Math.pow(256, 2) +
+    s3 * Math.pow(256, 1) +
+    s4 * Math.pow(256, 0);
+
+  const endCountFromZero =
+    e1 * Math.pow(256, 3) +
+    e2 * Math.pow(256, 2) +
+    e3 * Math.pow(256, 1) +
+    e4 * Math.pow(256, 0);
+
+  console.log({ startCountFromZero, endCountFromZero });
+  return endCountFromZero - startCountFromZero;
+}
+
+// Test
+
+// const start = "10.0.0.0";
+// const end = "10.0.0.50";
+// const start = "10.0.0.0";
+// const end = "10.0.1.0";
+const start = "20.0.0.10";
+const end = "20.0.1.0";
+console.log(ipsBetween(start, end));
---
+++ Typescript/src/Count IP Addresses/test/fill.test.ts
@@ -0,0 +1,11 @@
+import ipsBetween from "../index";
+
+describe("Count IP Addresses", () => {
+  it("Should return number of IP addresses between the 2 addresses.", () => {
+    const start = "20.0.0.10";
+    const end = "20.0.1.0";
+    const expectedResult = 246;
+
+    expect(ipsBetween(start, end).toString()).toBe(expectedResult);
+  });
+});
