14fee54519f035d14cec01cc4bd8d406f4991642
gnopor <mayoublaise@gmail.com>
2026-03-14 12:53:33 +0100

    Add new challenge: Stalin Sort

---
+++ src/Stalin Sort/index.ts
@@ -0,0 +1,32 @@
+export function stalinSort(array: number[]): void {
+  if (array.length == 0) return;
+  let finished = false;
+  while (true) {
+    for (let i = 1; i < array.length; i++) {
+      finished = i == array.length - 1;
+      console.log({ arrayLength: array.length, i, verdict: finished });
+
+      if (array[i] >= array[i - 1]) continue;
+      array.splice(i, 1);
+      break;
+    }
+
+    if (finished) break;
+  }
+
+  console.log("The end");
+}
+
+const params = [
+  { array: [1, 2, 3, 4, 5] },
+  { array: [5, 3, 1, 2, 4] },
+  { array: [3, 1, 4, 1, 5, 9, 2] },
+  { array: [1, 2, 2, 3, 1, 4] },
+  { array: [] },
+];
+
+for (let p of params) {
+  console.log(
+    `- input: ${JSON.stringify(p)} \n- ouput: ${stalinSort(p.array)}\n`,
+  );
+}
---
+++ src/Stalin Sort/test/stalinSort.test.ts
@@ -0,0 +1,26 @@
+import { stalinSort } from "../index";
+
+describe("Stalin Sort", () => {
+  it("Should sort the elements in the array using the Stalin Sort algorithm", () => {
+    const tests = [
+      {
+        params: { arr: [] },
+        output: [],
+      },
+      {
+        params: { arr: [3, 1, 4, 1, 5, 9, 2] },
+        output: [3, 4, 5, 9],
+      },
+      {
+        params: { arr: [5, 3, 1, 2, 4] },
+        output: [5],
+      },
+    ];
+
+    for (let { params, output } of tests) {
+      stalinSort(params.arr);
+      expect(params.arr).toBe(params.arr);
+      expect(params.arr.toString()).toBe(output.toString());
+    }
+  });
+});
